53d8ab8f9a133169553f6730fedef479339a21d6
aqideh <123452394+aqideh@users.noreply.github.com>
2026-09-03 10:58:17 +0800

    feat: wire event search and details editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./name-preference-toggle.css";
 import "./general-messaging.css";
 import "./sidebar-fix.css";
 import NamePreferenceToggle from "./name-preference-toggle";
+import SidebarEventSearch from "./sidebar-event-search";
 
 export const metadata: Metadata = {
   title: "Volunteer Message Tool",
@@ -14,11 +15,12 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body>
+        <SidebarEventSearch />
         <div className="floating-tools">
           <NamePreferenceToggle />
           <a className="general-messages-shortcut" href="/general">General messages</a>
           <a className="general-messages-shortcut" href="/roster-flags">Roster flags</a>
-          <a className="event-settings-shortcut" href="/event-details">Event links</a>
+          <a className="event-settings-shortcut" href="/event-details">Event details</a>
         </div>
         {children}
       </body>
